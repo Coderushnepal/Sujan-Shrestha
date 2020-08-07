b9261224130b2914da5325924a0ce54493132a33
Sujan Shrestha <shrsujan2007@gmail.com>
2020-08-07 07:55:24 +0545

    add build for github pages
    - remove build from .gitignore and generate it
    - change path on scss
    - add nodes-sass for sass compilation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./public";
+
 import App from "./components/App";
 
 ReactDOM.render(
--- src/public.js
+++ src/public.js
@@ -1,5 +1,5 @@
 // Application CSS
-import "./assets/sass/style.css";
+import "./assets/sass/style.scss";
 import "./assets/css/toaster.css";
 import "./assets/css/spinner.css";
 
