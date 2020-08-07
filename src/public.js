fbcea8c903253a00b89f7053c73989e1594d4d74
Sujan Shrestha <shrsujan2007@gmail.com>
2020-08-07 10:35:40 +0545

    deploy build for github pages

--- src/public.js
+++
@@ -1,7 +0,0 @@
-// Application CSS
-import "./assets/sass/style.scss";
-import "./assets/css/toaster.css";
-import "./assets/css/spinner.css";
-
-// Library dependent CSS
-import "izitoast/dist/css/iziToast.css";
